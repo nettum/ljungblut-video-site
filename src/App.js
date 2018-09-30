53ba0e852d6aa536e4652c8868d18847b5ed0633
Marius Nettum <marius.nettum@apt.no>
2018-09-30 23:08:02 +0200

    Add react-youtube and play logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 import './App.css';
 import videos from './videos.json';
-
 import Video from './Video.js';
 
 class App extends Component {
@@ -9,24 +9,52 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isPlaying: false,
+      videoId: null,
     }
   }
 
+  onVideoReady(event) {
+    console.log('ready');
+    event.target.playVideo();
+  }
+
   startVideo = (id) => {
+    console.log('start');
     this.setState({
-      isPlaying: true,
+      videoId: id,
     });
   };
 
+  resetVideoId = () => {
+    console.log('reset');
+    this.setState({
+      videoId: null,
+    });
+  };
+
+  renderVideo() {
+    const { videoId } = this.state;
+    return (
+      videoId !== null ?
+        <div className="youtube-wrapper">
+          <YouTube 
+            videoId={videoId}
+            onReady={this.onVideoReady}
+            onPause={this.resetVideoId}
+            onEnd={this.resetVideoId}
+          />
+        </div>
+      : null
+    );
+  }
+
   render() {
-    const { isPlaying } = this.state;
     return (
       <div className="ljungblut-videos">
         <div className="item">Ljungblut</div>
         {videos.map(video => <Video onClick={this.startVideo} key={video.id} {...video} />)}
         <div className="item">Ikke alle netter er like sorte</div>
-        {isPlaying && <div className="youtube-wrapper">PLAYER WRAPPER</div>}
+        {this.renderVideo()}
       </div>
     );
   }
