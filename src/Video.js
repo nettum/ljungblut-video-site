0b3bdf70e35b16078e3c0ef78d4e744a2dbaec64
Marius Nettum <nettum@gmail.com>
2021-09-02 13:11:56 +0200

    Simplify image loading and make Video a functional component

--- src/Video.js
+++ src/Video.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
 import './Video.css';
 
-class Video extends Component {
-  render() {
-    const { id, title, image, onClick } = this.props;
-    const bgStyle = {
-      backgroundImage: `url(${require(`./images/${image}`)}`
-    }
-    return (
-      <div className="item video" style={bgStyle} onClick={() => onClick(id)}>
-          <div>{title}</div>
-      </div>
-    );
+const Video = props => {
+  const { id, title, image, onClick } = props;
+
+  const bgStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/${image})`
   }
+
+  return (
+    <div className="item video" style={bgStyle} onClick={() => onClick(id)}>
+        <div>{title}</div>
+    </div>
+  );
 }
 
 export default Video;
